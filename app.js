dc8e57214e0600ca1d0816cbf9dcffa94b5ddd81
supra321 <suprativdatta@gmail.com>
2020-09-07 22:07:29 +0530

    PORT fixed

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.get("/posts/:postId", (req, res) => {
 	});
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
